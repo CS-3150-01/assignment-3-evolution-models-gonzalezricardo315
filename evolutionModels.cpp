cd562b3e741252f8e4c1caad14bb1b77fb58fd10
gonzalezricardo315 <Gonzalezricardo315@yahoo.com>
2022-10-28 16:34:50 -0600

    initial commit of cpp file, wirefram.

---
+++ evolutionModels.cpp
@@ -0,0 +1,35 @@
+class World{
+    private: Creature createCreature(){
+
+    }
+};
+
+class Creature {
+    private: int health;
+    private: Creature createCreature(){
+
+    }
+    private: void die(){
+        //if creatue health is low
+    }
+    private: Creature reproduce(){
+        //will make another creature
+    }
+};
+
+class Action {
+    private: Creature createCrature(){
+        //createCreature
+    }
+    private: int attack(){
+        //return int damage the action causes
+    }
+    private: int** move(){
+        //return pos[x][y] in 2d array
+    }
+};
+
+int main(){
+    //loop for rolling 
+    return 0;
+}
